73e9ef9b126de798247124c9074fb40c6388feb3
compujuckel <compujuckel@gmail.com>
2014-08-23 18:07:42 +0200

    add remeber_token column in user table

---
+++ app/database/migrations/2014_08_23_160223_users_add_remember_token_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersAddRememberTokenColumn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->text('remember_token')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->dropColumn('remember_token');
+		});
+	}
+
+}
