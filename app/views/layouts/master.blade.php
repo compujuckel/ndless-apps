e88406fc2df94a677f418af53dc5c3ed48db3ceb
compujuckel <compujuckel@gmail.com>
2014-11-03 19:45:08 +0100

    add favicon

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="/favicon.png" rel="icon" type="image/png">
 		<link href="/css/bootstrap.min.css" rel="stylesheet">
 		<link href="/css/font-awesome.min.css" rel="stylesheet">
 		@if(App::environment() == "euve34239")
